debe31d89d9ba3039425b96778c4afaafe5d72e5
arene-ri <arene-ri@student.42malaga.com>
2025-11-17 13:21:53 +0100

    Carpeta con los ejercicios corregidos en el campus

--- campus/libft/ft_memset.c
+++ campus/libft/ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: arene-ri <arene-ri@student.42malaga.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/17 11:45:18 by arene-ri          #+#    #+#             */
-/*   Updated: 2025/11/17 11:45:23 by arene-ri         ###   ########.fr       */
+/*   Updated: 2025/11/17 13:06:15 by arene-ri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,12 @@ void	*ft_memset(void *str, int c, size_t n)
 	}
 	return (str);
 }
+
+// 1. Convertimos el char que recibimos en unsiged
+// 2. Lo guardamos en una variable auxiliar
+// 3. Inicializamos un contador
+// 4. Creamos un bucle que recorra el puntero y añada un valor concreto en cada posición hasta llegar a la posición n
+// 5. Devolvemos el string modificado 
 
 int	main(void)
 {
