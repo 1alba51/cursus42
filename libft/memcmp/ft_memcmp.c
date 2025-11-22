90002a8a1363ec4bc393bbcf58fd7f7761788519
Alba <renuricoalba@gmail.com>
2025-11-22 12:19:26 +0000

    Función memcmp sin terminar

---
+++ libft/memcmp/ft_memcmp.c
@@ -0,0 +1,10 @@
+// Esta función compara n bytes del string1 con el string2 y devuelve la diferencia de los valores en ASCII.
+// El prototipo de la función será:
+// 		int	ft_memcmp(const void *s1, const void *s2, size_t n);
+
+#include <stdio.h>
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	
+}
