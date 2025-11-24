dedde48ba371117c892a53aab0d08df5ff179910
Alba <renuricoalba@gmail.com>
2025-11-24 12:06:52 +0000

    Función calloc sin terminar

---
+++ libft/calloc/ft_calloc.c
@@ -0,0 +1,18 @@
+// Esta función reserva espacio de almacenamiento para una matriz de elementos num cada uno de ellos de longitud
+// de tamaño bytes.
+// Proporciona a todos los bits de cada elemento un valor inicial de 0
+// Devuelve un puntero al espacio reservado.
+// El prototipo de la función será:
+// 			void	*ft_calloc(size_t nmemb, size_t size);
+
+#include <stdio.h>
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	
+}
+
+int	main(void)
+{
+	return(0);
+}
