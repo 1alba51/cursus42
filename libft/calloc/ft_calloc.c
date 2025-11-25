64a94f939d19801416d4fc47b2e728e39a68bb95
Alba <renuricoalba@gmail.com>
2025-11-25 11:49:10 +0000

    Función calloc explicada y comprobada

--- libft/calloc/ft_calloc.c
+++ libft/calloc/ft_calloc.c
@@ -11,10 +11,6 @@
 
 void	*ft_calloc(size_t nmemb, size_t size)
 {
-	if (nmemb == 0 || size == 0)
-	{
-		return(NULL);
-	}
 	size_t	total_bytes;
 	void	*ptr;
 	size_t	i;
@@ -34,21 +30,22 @@ void	*ft_calloc(size_t nmemb, size_t size)
 	return((unsigned char *)ptr);
 }
 
+// 1. Creamos una variable que guarde el valor del resultado que devuelva la función malloc()
+// 2. Esta va a calcular el tamaño que tendrán los elementos dados, por lo que es igual al número de elementos multiplicado por el tamaño de estos
+// 3. Llamamos a la función malloc()
+// 4. Si el resultado es 0 devuelve NULL
+// 5. Recorremos el bloque de memoria sin superar su tamaño total
+// 6. Añadimos 0 hasta rellenarlo
+// 7. Devolvemos los elementos con valorlos elementos con valor 0 cada unoo
+
 int	main(void)
 {
-	size_t	num = 0;
+	size_t	num = 3;
 	size_t	len = sizeof(int);
 	int	*ptr;
 
-	if (ptr == NULL)
-	{
-		printf("Error: no hay memoria disponible");
-	}
-	else
-	{
-		ptr = ft_calloc(num, len);
-		printf("Calloc: %d %d %d\n", ptr[0], ptr[1], ptr[2]);
-	}
+	ptr = ft_calloc(num, len);
+	printf("Calloc: %d %d %d\n", ptr[0], ptr[1], ptr[2]);
 	free(ptr);
 	return(0);
 }
