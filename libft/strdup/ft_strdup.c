06ebed3229800403651ea9cd896c025485dcedab
Alba <renuricoalba@gmail.com>
2025-12-01 21:23:40 +0000

    Función strdup explicada y comprobada

--- libft/strdup/ft_strdup.c
+++ libft/strdup/ft_strdup.c
@@ -10,23 +10,50 @@
 
 char	*ft_strdup(const char *s)
 {
-	size_t	total_bytes;
-	char	*ptr;
+	size_t	len;
 	size_t	i;
+	char	*dst;
 
-	total_bytes = 
+	len = 0;
+	i = 0;
+	while (s[len])
+	{
+		len++;
+	}
+	dst = malloc(len + 1);
+	if (dst == NULL)
+	{
+		return (NULL);
+	}
+	while (s[i] != '\0')
+	{
+		dst[i] = s[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (dst);
 }
 
-// 1. 
+// 1. Calcular la longitud del string original sin contar \0
+// 2. Reservamos memoria con malloc() para longitud + 1 (\0)
+// 3. Comprobamos si malloc() devuelve NULL
+// 4. Copia caracteres incluyendo el \0
 
 int	main(void)
 {
-	char	original[] = "Hello World!";
 	char	*newstring;
 
-	printf("Original: %s\n", original);
-	newstring = ft_strdup(original);
-	printf("Copia: %s\n", newstring);
+	newstring = ft_strdup("Hello World!");
+	printf("Copia 1: %s\n\n", newstring);
 	free(newstring);
+
+	newstring = ft_strdup("");
+	printf("Copia 2: %s\n\n", newstring);
+	free(newstring);
+
+	newstring = ft_strdup("A");
+	printf("Copia 3: %s\n\n", newstring);
+	free(newstring);
+
 	return(0);
 }
