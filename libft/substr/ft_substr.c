7f53fbb9dad8bb2b04aa5c3f7adbe7ba42daded0
Alba <renuricoalba@gmail.com>
2025-12-01 21:35:29 +0000

    Función substr sin terminar

---
+++ libft/substr/ft_substr.c
@@ -0,0 +1,21 @@
+// Esta función reserva (con malloc (3)) y devuelve una substring de la string 's'.
+// La substring empieza desde el índice 'start' y tiene una longitud máxima 'len'.
+// s: la string desde la que crear la substring
+// start: el índice del caracter en 's' desde el que empezar la substring
+// len: la longitud máxima de la substring
+// El prototipo de la función será:
+// 		char	*ft_substr(char const *s, unsigned int start, size_t len);
+
+#include <stdio.h>
+#include <malloc.h>
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+
+}
+
+int	main(void)
+{
+	fre();
+	return(0);
+}
