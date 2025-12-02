06f0357df6ece9de2e9ee9a86b9343d2cd721723
Alba <renuricoalba@gmail.com>
2025-12-02 10:58:28 +0000

    Función strjoin sin empezar

---
+++ libft/strjoin/ft_strjoin.c
@@ -0,0 +1,21 @@
+// Reserva (con malloc(3)) y devuelve una nueva string, formada por la concatenación de 's1' y 's2'.
+// PARÁMETROS: 
+// s1: la primera string
+// s2: la string a añadir a 's1'
+// DEVUELVE:
+// la nueva string. NULL si falla la reserva de memoria
+// El prototipo de la función será:
+//			char *ft_strjoin(char const *s1, char const *s2);
+
+#include <stdio.h>
+#include <malloc.h>
+
+char	*ft_strjoin(char const *s1, const char *s2)
+{
+	
+}
+
+int	main(void)
+{
+	
+}
